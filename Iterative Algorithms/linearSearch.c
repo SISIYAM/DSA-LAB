e8338d109834c85c9d05b08c250476748ac0300d
MD Saymum Islam Siyam <si31siyam@gmail.com>
2024-11-07 03:07:04 +0600

    Iterative algorithm

---
+++ Iterative Algorithms/linearSearch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;  // Return the index of the target
+        }
+    }
+    return -1;  // Target not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 6;
+
+    int result = linearSearch(arr, size, target);
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found in the array\n");
+    }
+    return 0;
+}
